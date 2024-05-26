e5286bce8abddac4bceb7c558e3e800c46a2a72b
Dharaneesh0745 <dharaneesh0745@gmail.com>
2024-05-26 20:57:26 +0530

    Build cache updated using command.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "import.meta.env.VITE_FIREBASE_API_KEY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "presidio-rentify.firebaseapp.com",
   projectId: "presidio-rentify",
   storageBucket: "presidio-rentify.appspot.com",
