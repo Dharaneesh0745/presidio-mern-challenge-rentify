20a9f08524157210a11119b4702a9b39ddb44946
Dharaneesh0745 <dharaneesh0745@gmail.com>
2024-05-26 21:06:50 +0530

    Updated firebase API for authentication and fixed social OAuth issues and errors.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyA-lF64MTQmGmxxgxQOfdnN0c7dbfLwrNM",
   authDomain: "presidio-rentify.firebaseapp.com",
   projectId: "presidio-rentify",
   storageBucket: "presidio-rentify.appspot.com",
